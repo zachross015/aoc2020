ed22194c620ba22aa29d10a5b63d47ad0c9c0b75
Zachary Ross <zachross015@gmail.com>
2020-12-01 01:14:02 -0500

    Modifications

--- day1.hs
+++ day1.hs
@@ -1,23 +1,17 @@
 convert :: String -> [Int]
 convert = map read . lines
 
-genTuples2 :: [Int] -> [(Int, Int)]
-genTuples2 x = [(y1, y2) | y1 <- x, y2 <- x]
+genTuples2 :: [Int] -> [[Int]]
+genTuples2 x = [[y1, y2] | y1 <- x, y2 <- x]
 
-genTuples3 :: [Int] -> [(Int, Int, Int)]
-genTuples3 x = [(y1, y2, y3) | y1 <- x, y2 <- x, y3 <- x]
+genTuples3 :: [Int] -> [[Int]]
+genTuples3 x = [[y1, y2, y3] | y1 <- x, y2 <- x, y3 <- x]
 
-part1 :: [Int] -> Int
-part1 = part1' . head . filter (\(x, y) -> x + y == 2020) . genTuples2
-    where part1' (x, y) = x * y
-
-part2 :: [Int] -> Int
-part2 = part1' . head . filter (\(x, y, z) -> x + y + z == 2020) . genTuples3
-    where part1' (x, y, z) = x * y * z
-
+day1 :: ([Int] -> [[Int]]) -> [Int] -> Int
+day1 g = product . head . filter (\x -> sum x == 2020) . g
 
 main = do
-    contents <- readFile "inputs/day1"
-    putStrLn $ show . part1 . convert $ contents
-    putStrLn $ show . part2 . convert $ contents
+    contents <- readFile "inputs/day1.txt"
+    putStrLn $ show . day1 genTuples2 . convert $ contents
+    putStrLn $ show . day1 genTuples3 . convert $ contents
 

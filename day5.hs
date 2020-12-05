83ffeb04de70b830163237f6100b10858c69dc5c
Zachary Ross <zachross015@gmail.com>
2020-12-05 00:51:26 -0500

    Optimization

--- day5.hs
+++ day5.hs
@@ -3,14 +3,11 @@ import Data.List
 binaryArrayToInt :: [Int] -> Int
 binaryArrayToInt = foldl (\acc x -> acc * 2 + x) 0
 
-row :: String -> Int
-row = binaryArrayToInt . map (\c -> if c == 'F' then 0 else 1) . take 7
-
-column :: String -> Int
-column = binaryArrayToInt . map (\c -> if c == 'L' then 0 else 1) . drop 7
-
 seatNumber :: String -> Int
-seatNumber line = (row line) * 8 + (column line)
+seatNumber line = row * 8 + column
+    where bArr = map (\c -> if c == 'F' || c == 'L' then 0 else 1) line
+          row = binaryArrayToInt . take 7 $ bArr
+          column = binaryArrayToInt . drop 7 $ bArr
 
 seats :: String -> [Int]
 seats = map seatNumber . lines
@@ -19,9 +16,8 @@ part1 :: String -> Int
 part1 = maximum . seats
 
 part2 :: String -> Int
-part2 inp = head $ [n..m] \\ seats inp
-    where m = maximum . seats $ inp
-          n = minimum . seats $ inp
+part2 inp = head $ [(minimum s)..(maximum s)] \\ s
+    where s = seats inp
 
 main = do
     contents <- readFile "inputs/day5.txt"

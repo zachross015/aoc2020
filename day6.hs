eeac52471068a3412a51152241b38b4e5e60960d
Zachary Ross <zachross015@gmail.com>
2020-12-06 02:26:03 -0500

    Adjustments

--- day6.hs
+++ day6.hs
@@ -2,14 +2,15 @@ import Data.List.Split (splitOn)
 import Data.Set (Set)
 import qualified Data.Set as S
 
+structureList f init p = foldr (\x acc -> f (S.fromList x) acc) init p
+
+day6 f = sum . map (S.size . f) . map lines . splitOn "\n\n"
+
 part1 :: [String] -> Set Char
-part1 p = foldr (\x acc -> S.union (S.fromList x) acc) S.empty p
+part1 = structureList S.union S.empty
 
 part2 :: [String] -> Set Char
-part2 p = foldr (\x acc -> S.intersection (S.fromList x) acc) (S.fromList ['a'..'z']) p
-
-input = map lines . splitOn "\n\n"
-day6 f = sum . map (S.size . f) . input
+part2 = structureList S.intersection (S.fromList ['a'..'z'])
 
 main = do 
     contents <- readFile "inputs/day6.txt"

f190f251a697cd8b5acc00da1d380d2e09e58e32
Zachary Ross <zachross015@gmail.com>
2020-12-06 14:06:47 -0500

    Spring cleaning

--- day6.hs
+++ day6.hs
@@ -2,15 +2,17 @@ import Data.List.Split (splitOn)
 import Data.Set (Set)
 import qualified Data.Set as S
 
-structureList f init p = foldr (\x acc -> f (S.fromList x) acc) init p
+structureList :: (Ord a) => (Set a -> Set a -> Set a) -> [a] -> [[a]] -> Set a
+structureList f i p = foldr f (S.fromList i) (map S.fromList p)
 
+day6 :: ([String] -> Set Char) -> String -> Int
 day6 f = sum . map (S.size . f) . map lines . splitOn "\n\n"
 
 part1 :: [String] -> Set Char
-part1 = structureList S.union S.empty
+part1 = structureList S.union []
 
 part2 :: [String] -> Set Char
-part2 = structureList S.intersection (S.fromList ['a'..'z'])
+part2 = structureList S.intersection ['a'..'z']
 
 main = do 
     contents <- readFile "inputs/day6.txt"

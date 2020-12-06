c2ce7abfb2c06932b0c3e3069ea4d5aac802452f
Zachary Ross <zachross015@gmail.com>
2020-12-06 02:21:21 -0500

    CLeaning up

--- day6.hs
+++ day6.hs
@@ -2,22 +2,16 @@ import Data.List.Split (splitOn)
 import Data.Set (Set)
 import qualified Data.Set as S
 
+part1 :: [String] -> Set Char
+part1 p = foldr (\x acc -> S.union (S.fromList x) acc) S.empty p
+
+part2 :: [String] -> Set Char
+part2 p = foldr (\x acc -> S.intersection (S.fromList x) acc) (S.fromList ['a'..'z']) p
 
 input = map lines . splitOn "\n\n"
-
-validatedPuzzles :: [String] -> Set Char
-validatedPuzzles p = foldr (\x acc -> S.union (S.fromList x) acc) S.empty p
-
-validatedPuzzles2 :: [String] -> Set Char
-validatedPuzzles2 p = foldr (\x acc -> S.intersection (S.fromList x) acc) (S.fromList ['a'..'z']) p
-
-part1 :: String -> Int
-part1 = sum . map (S.size . validatedPuzzles) . input
-
-part2 :: String -> Int
-part2 = sum . map (S.size . validatedPuzzles2) . input
+day6 f = sum . map (S.size . f) . input
 
 main = do 
     contents <- readFile "inputs/day6.txt"
-    print $ part1 contents
-    print $ part2 contents
+    print $ day6 part1 contents
+    print $ day6 part2 contents

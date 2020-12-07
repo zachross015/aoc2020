c5adb0ecfd44d3514458bfd809b45b440f826dc6
Zachary Ross <zachross015@gmail.com>
2020-12-07 01:35:29 -0500

    Cleaned up a bit

--- day7.hs
+++ day7.hs
@@ -6,19 +6,20 @@ import Data.List
 
 parseBag :: [String] -> [(Int, String)]
 parseBag (["no", "other", "bags."]) = []
-parseBag (x1:x2:x3:x4:xs) = ((read x1), x2 ++ " " ++ x3):(if last x4 == ',' then (parseBag xs) else []) 
+parseBag (x1:x2:x3:x4:xs) = (read x1, x2 ++ " " ++ x3):(if last x4 == ',' then (parseBag xs) else []) 
 
 makeBags :: String -> Map String [(Int, String)]
 makeBags s = foldr M.union M.empty . map (makeBag' . words) . lines $ s
     where makeBag' s = M.singleton (intercalate " " . take 2 $ s) (parseBag . drop 4 $ s)
 
 transitiveClosure :: String -> Map String [(Int, String)] -> Set String
-transitiveClosure s m = (S.fromList directBags) `S.union` (foldr closureFold S.empty directBags)
+transitiveClosure s m = S.union (S.fromList directBags) (foldr closureFold S.empty directBags)
     where directBags = map snd (m M.! s)
-          closureFold x acc = acc `S.union` transitiveClosure x m
+          closureFold x acc = S.union acc $ transitiveClosure x m
 
-howManyCanHold :: String -> Map String [(Int, String)] -> Int
-howManyCanHold bag m = sum $ map (\x -> if bag `S.member` (transitiveClosure x m) then 1 else 0) (M.keys m)
+howManyHold :: String -> Map String [(Int, String)] -> Int
+howManyHold bag m = sum $ map isInTClosure (M.keys m)
+    where isInTClosure x = if S.member bag (transitiveClosure x m) then 1 else 0
 
 howManyDoesItHold :: String -> Map String [(Int, String)] -> Int
 howManyDoesItHold bag m = foldr howManyDoesItHold' 0 (m M.! bag)
@@ -26,5 +27,5 @@ howManyDoesItHold bag m = foldr howManyDoesItHold' 0 (m M.! bag)
 
 main = do
     contents <- readFile "inputs/day7.txt"
-    print $ howManyCanHold "shiny gold" (makeBags contents)
+    print $ howManyHold "shiny gold" (makeBags contents)
     print $ howManyDoesItHold "shiny gold" (makeBags contents)
